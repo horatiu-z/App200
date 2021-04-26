976e8e0bef42858ad1ce151d685adf704715862c
Horatiu Zatoschil <horatiu.zatoschil@gmail.com>
2021-04-26 21:51:44 +0300

    Added a list of steps to take, and a nice way to print the steps that where taken

--- program.py
+++ program.py
@@ -14,18 +14,27 @@ def insert_value():
 """Now for the main program, we want to divide the number to 2 as many times as posible, and substract 1 only when it's not possible to divide by 2"""
 
 def moves_to_make(sum):
+    """Create a list of stept so take, and reverse it at the end"""
+    list_of_steps=[]
     count = 0
     while sum > 1:
         if sum % 2 == 0:
             sum = sum / 2
+            list_of_steps.append("Multiply {}$ with 2 = {}$".format(sum,sum*2))
             count += 1
         else:
             sum = sum-1
+            list_of_steps.append("Added 1$ to {}$ = {}$".format(sum,sum+1))
             count += 1
-    return count
+    return count, reversed(list_of_steps)
 
 """ Now, let's just execute the program"""
 
 if __name__ == '__main__':
     sum = insert_value()
-    print(moves_to_make(sum))
+    steps, moves = moves_to_make(sum)
+    print("{} steps as follows:".format(steps))
+    y = 1
+    for move in moves:
+        print("step {}. {}".format(y,move))
+        y += 1
